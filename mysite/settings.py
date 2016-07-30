ed777b193b42068c0affbb9bb190afdb6f630397
lheston <loomisheston@hotmail.com>
2016-07-30 00:46:52 +0000

    fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'lheston',
+        'PASSWORD': 'Loomloom1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -119,6 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/blog/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
